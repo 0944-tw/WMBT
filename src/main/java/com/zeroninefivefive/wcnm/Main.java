be3e3776e6bd41f9ac7afd5d758744ccd8788ae1
zeroninefourfour <zwec181818@proton.me>
2025-06-21 09:23:53 +0800

    Main code

---
+++ src/main/java/com/zeroninefivefive/wcnm/Main.java
@@ -0,0 +1,102 @@
+package com.zeroninefivefive.wcnm;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.zeroninefivefive.wcnm.Arena.ArenaEvents;
+import com.zeroninefivefive.wcnm.Arena.ArenaManager;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import net.kyori.adventure.text.Component;
+import net.megavex.scoreboardlibrary.api.ScoreboardLibrary;
+import net.megavex.scoreboardlibrary.api.exception.NoPacketAdapterAvailableException;
+import net.megavex.scoreboardlibrary.api.noop.NoopScoreboardLibrary;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mvplugins.multiverse.core.MultiverseCoreApi;
+import org.mvplugins.multiverse.core.world.MultiverseWorld;
+import org.mvplugins.multiverse.core.world.options.DeleteWorldOptions;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class Main extends JavaPlugin {
+
+    public ArenaManager arenaManager;
+    public ArenaEvents arenaEvents;
+    public ScoreboardLibrary Scoreboard;
+    @Override
+    @SuppressWarnings("UnstableApiUsage")
+    public void onEnable() {
+        // Welcome to OSU
+        saveResource("eh_maps.yml", /* replace */ false);
+        File dataFolder = new File(getDataFolder(), "maps");
+        this.arenaManager = new ArenaManager(this);
+
+        if (!dataFolder.exists()) {
+            boolean created = dataFolder.mkdirs(); // Creates folders, including parents
+            if (created) {
+                getLogger().info("Map folder created.");
+            } else {
+                getLogger().warning("Failed to create maps folder.");
+            }
+        }
+        Bukkit.getScheduler().runTaskLater(this, bukkitTask -> {
+            MultiverseCoreApi coreApi = MultiverseCoreApi.get();
+            for (MultiverseWorld world : coreApi.getWorldManager().getWorlds()) {
+                if (world.getName().startsWith("wmbt_")) {
+                    getLogger().info("Deleteting Old World " + world.getName());
+                    coreApi.getWorldManager().deleteWorld(DeleteWorldOptions.world(world));
+                }
+            }
+        },25L);
+
+           try {
+            Scoreboard = ScoreboardLibrary.loadScoreboardLibrary(this);
+        } catch (NoPacketAdapterAvailableException e) {
+             Scoreboard = new NoopScoreboardLibrary();
+            getLogger().warning("no scoreboard library found!!! call 911 asap as soon as soup!!!!!");
+        }
+
+        this.arenaEvents = new ArenaEvents(this);
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, command -> {
+            LiteralArgumentBuilder<CommandSourceStack> startgame_cmd = Commands.literal("gamestart")
+                    .requires(player -> player.getSender().isOp())
+                    .executes(ctx -> {
+                        try {
+                            String ServerUUID = arenaManager.CreateGameLobby(null);
+                            ctx.getSource().getSender().sendMessage(Component.text(ServerUUID));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return Command.SINGLE_SUCCESS;
+                    })
+                    .then(Commands.literal("players"))
+                    .then(Commands.literal("map"));
+            LiteralArgumentBuilder<CommandSourceStack> GameJoin = Commands.literal("join")
+                    .then(
+                            Commands.argument("lobby", StringArgumentType.greedyString())
+                                    .executes(ctx -> {
+                                        arenaManager.JoinGame((Player) ctx.getSource().getSender(),ctx.getArgument("lobby",String.class));
+                                        return Command.SINGLE_SUCCESS;
+                                    })
+                    );
+
+             command.registrar().register(startgame_cmd.build());
+            command.registrar().register(GameJoin.build());
+        });
+    }
+
+    @Override
+    public void onDisable() {
+        MultiverseCoreApi coreApi = MultiverseCoreApi.get();
+        for (MultiverseWorld world : coreApi.getWorldManager().getWorlds()) {
+            if (world.getName().startsWith("wmbt_")) {
+                getLogger().info("Deleteting Old World " + world.getName());
+                coreApi.getWorldManager().deleteWorld(DeleteWorldOptions.world(world));
+            }
+        }
+    }
+}
