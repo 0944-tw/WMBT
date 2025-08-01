bf1170a53cf00e44df7d2d5e6052033244f3a0c2
zeroninefourfour <zwec181818@proton.me>
2025-08-01 10:34:16 +0800

    Rename commands, Add lobby, players args

--- src/main/java/com/zeroninefivefive/wcnm/Main.java
+++ src/main/java/com/zeroninefivefive/wcnm/Main.java
@@ -7,8 +7,12 @@ import com.zeroninefivefive.wcnm.Arena.ArenaEvents;
 import com.zeroninefivefive.wcnm.Arena.ArenaManager;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import net.kyori.adventure.Adventure;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.megavex.scoreboardlibrary.api.ScoreboardLibrary;
 import net.megavex.scoreboardlibrary.api.exception.NoPacketAdapterAvailableException;
 import net.megavex.scoreboardlibrary.api.noop.NoopScoreboardLibrary;
@@ -21,12 +25,14 @@ import org.mvplugins.multiverse.core.world.options.DeleteWorldOptions;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public final class Main extends JavaPlugin {
 
     public ArenaManager arenaManager;
     public ArenaEvents arenaEvents;
     public ScoreboardLibrary Scoreboard;
+
     @Override
     @SuppressWarnings("UnstableApiUsage")
     public void onEnable() {
@@ -51,19 +57,21 @@ public final class Main extends JavaPlugin {
                     coreApi.getWorldManager().deleteWorld(DeleteWorldOptions.world(world));
                 }
             }
-        },25L);
+        }, 25L);
 
-           try {
+        try {
             Scoreboard = ScoreboardLibrary.loadScoreboardLibrary(this);
         } catch (NoPacketAdapterAvailableException e) {
-             Scoreboard = new NoopScoreboardLibrary();
+            Scoreboard = new NoopScoreboardLibrary();
             getLogger().warning("no scoreboard library found!!! call 911 asap as soon as soup!!!!!");
         }
 
         this.arenaEvents = new ArenaEvents(this);
         this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, command -> {
-            LiteralArgumentBuilder<CommandSourceStack> startgame_cmd = Commands.literal("gamestart")
+            LiteralArgumentBuilder<CommandSourceStack> startgame_cmd = Commands.literal("start")
                     .requires(player -> player.getSender().isOp())
+                    .then(Commands.literal("players"))
+                    .then(Commands.literal("map"))
                     .executes(ctx -> {
                         try {
                             String ServerUUID = arenaManager.CreateGameLobby(null);
@@ -72,19 +80,37 @@ public final class Main extends JavaPlugin {
                             throw new RuntimeException(e);
                         }
                         return Command.SINGLE_SUCCESS;
-                    })
-                    .then(Commands.literal("players"))
-                    .then(Commands.literal("map"));
-            LiteralArgumentBuilder<CommandSourceStack> GameJoin = Commands.literal("join")
-                    .then(
-                            Commands.argument("lobby", StringArgumentType.greedyString())
-                                    .executes(ctx -> {
-                                        arenaManager.JoinGame((Player) ctx.getSource().getSender(),ctx.getArgument("lobby",String.class));
-                                        return Command.SINGLE_SUCCESS;
-                                    })
-                    );
+                    });
 
-             command.registrar().register(startgame_cmd.build());
+            LiteralArgumentBuilder<CommandSourceStack> GameJoin = Commands.literal("join")
+                    .then(Commands.argument("lobby", StringArgumentType.string()))
+                    .then(Commands.argument("players", ArgumentTypes.players()))
+                    .executes(ctx -> {
+                        final PlayerSelectorArgumentResolver targetResolver = ctx.getArgument("players", PlayerSelectorArgumentResolver.class);
+                        final List<Player> targets = targetResolver.resolve(ctx.getSource());
+                        String ArenaUUID = ctx.getArgument("lobby", String.class);
+                        if (targets.size() > 1 & !ctx.getSource().getSender().isOp()) {
+                            ctx.getSource().getSender().sendMessage("go die!!! you are not op!!!!!! you cant do this you stupid im going to ban you!!!!");
+                            return Command.SINGLE_SUCCESS;
+                        }
+                        if (!targets.isEmpty()) {
+                            ctx.getSource().getSender().sendMessage(Component.text("Players missing.").color(NamedTextColor.RED));
+                            return Command.SINGLE_SUCCESS;
+                        }
+                        if (arenaManager.GetArenaFromWorldUUID(ArenaUUID) != null) {
+                            targets.forEach( player -> {
+                                player.sendMessage(Component.text("Teleporting you to ").color(NamedTextColor.YELLOW).append(Component.text()));
+                                arenaManager.JoinGame(player, ctx.getArgument("lobby", String.class));
+                            });
+                        } else {
+                            ctx.getSource().getSender().sendMessage(Component.text("Server not exists!").color(NamedTextColor.RED));
+                        }
+
+                         return Command.SINGLE_SUCCESS;
+                    });
+
+
+            command.registrar().register(startgame_cmd.build());
             command.registrar().register(GameJoin.build());
         });
     }
